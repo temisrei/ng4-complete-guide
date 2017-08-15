bf12ce39200ee029395430af541deb0d7cb2f661
Rei Lin <temisrei@gmail.com>
2017-08-15 23:42:02 +0800

    搬移下層元件 RecipeListComponent 初始屬性值 到 RecipeService，並設定 getRecipes 讀取值的方法

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,12 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    // tslint:disable-next-line:max-line-length
-    new Recipe('1st Test Recipe', 'This is simply a [first] test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg'),
-    new Recipe('2nd Test Recipe', 'This is simply a [second] test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-05.jpg')
-  ];
-
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  private recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('1st Test Recipe', 'This is simply a [first] test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg'),
+    new Recipe('2nd Test Recipe', 'This is simply a [second] test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-05.jpg')
+  ];
 
   constructor() { }
 
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
